204caa3100b7d07c6dfce63d4612b586baae3179
seifmaalel <seifmaalel@live.fr>
2023-08-24 12:03:27 +0100

    mon premier commit

---
+++ api/config/db.config.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const logger = require('../logger/api.logger');
+
+const connect = () => {
+
+    const url = "mongodb://localhost:27017";
+    logger.info("process.env.MONGO_CONNECTION_STRING :::" + "mongodb://localhost:27017");
+
+
+    mongoose.connect(url, {
+        useNewUrlParser: true,
+        useFindAndModify: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+    })
+
+    mongoose.connection.once("open", async () => {
+        logger.info("Connected to database");
+    });
+      
+    mongoose.connection.on("error", (err) => {
+        logger.error("Error connecting to database  ", err);
+    });
+}
+
+const disconnect = () => {
+    
+    if (!mongoose.connection) {
+      return;
+    }
+    
+    mongoose.disconnect();
+
+    mongoose.once("close", async () => {
+        console.log("Diconnected  to database");
+    });
+
+};
+
+module.exports = {
+    connect,
+    disconnect
+}
